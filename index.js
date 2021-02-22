bd4a94dd69bf72899dc600c60217e049bbc1d57d
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-02-21 18:19:24 -0600

    commit tree b3141fc260aa7553dc7a4aaa066fb6dbbb9d69f1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'me';
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+    bestCustomer = 'J';
+}
+
+function leastFavoriteCustomer() {
+     leastFavoriteCustomer = 'bbob';
+}
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'mat';
+}
